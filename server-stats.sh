9f32dbeb84b233b947aba2128ebeb5fd19b9083a
PJ Maas <petrusjohannesmaas@outlook.com>
2025-04-30 14:20:08 +0200

    Disk usage and top 5 processes by CPU and memory to complete project requirements

--- server-stats.sh
+++ server-stats.sh
@@ -13,8 +13,37 @@ free_memory=$(echo "$memory_stats" | awk '{print $4 / 1024^2}')   # Convert to G
 used_memory_percentage=$(echo "scale=2; ($used_memory / $total_memory) * 100" | bc)
 free_memory_percentage=$(echo "scale=2; ($free_memory / $total_memory) * 100" | bc)
 
+# Total disk usage (Free vs Used including percentage)
+disk_stats=$(df -B1 --total | grep "total")
+
+total_disk=$(echo "$disk_stats" | awk '{print $2 / 1024^3}')
+used_disk=$(echo "$disk_stats" | awk '{print $3 / 1024^3}')
+free_disk=$(echo "$disk_stats" | awk '{print $4 / 1024^3}')
+
+used_disk_percentage=$(echo "scale=2; ($used_disk / $total_disk) * 100" | bc)
+
+# Top 5 processes by CPU usage
+top_cpu_processes=$(ps -eo comm,%cpu --sort=-%cpu | head -6 | tail -5)
+
+# Top 5 processes by memory usage (with "MEM" explicitly added)
+top_memory_processes=$(ps -eo pid,comm,%mem --sort=-%mem | head -6 | tail -5 | awk '{printf "%-20s MEM: %.2f%%\n", $2, $3}' | sort -k2 -nr
+)
+
 # Metrics output
+echo "Server stats:"
+echo "---"
 echo "Total CPU Usage: $cpu_usage %"
+echo "---"
 echo "Total Memory: $total_memory GiB"
 echo "Used Memory: $used_memory GiB ($used_memory_percentage %)"
 echo "Free Memory: $free_memory GiB ($free_memory_percentage %)"
+echo "---"
+echo "Total Disk Space: $total_disk GiB"
+echo "Used Disk Space: $used_disk GiB ($used_disk_percentage %)"
+echo "Free Disk Space: $free_disk GiB"
+echo "---"
+echo "Top 5 Processes by CPU Usage:"
+echo "$top_cpu_processes"
+echo "---"
+echo "Top 5 Processes by Memory Usage:"
+echo "$top_memory_processes"
